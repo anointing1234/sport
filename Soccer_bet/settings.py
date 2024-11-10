705b6f657359d90fb51dc2c1aa4c1f3c31db8eaa
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2024-11-10 03:46:30 +0100

    commit

--- Soccer_bet/settings.py
+++ Soccer_bet/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = 'django-insecure-*^x+y-u8wp0@mkj35p5g-5ge7ugiu)+5_&4ll6*#^j-cae(5#1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # DEBUG_PROPAGATE_EXCEPTIONS = True
-CSRF_TRUSTED_ORIGINS = ["https://sport-production.up.railway.app"]
-ALLOWED_HOSTS = ["sport-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://soccer247gv.com"]
+ALLOWED_HOSTS = ["soccer247gv.com"]
 # ALLOWED_HOSTS = ['*']
 
 # # # Application definition
