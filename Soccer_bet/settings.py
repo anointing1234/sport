a762dab7017af2eeebc968be6c23bdf504105934
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2024-11-20 13:46:09 +0100

    initail commmit

--- Soccer_bet/settings.py
+++ Soccer_bet/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR,'file.env'))
 SECRET_KEY = 'django-insecure-*^x+y-u8wp0@mkj35p5g-5ge7ugiu)+5_&4ll6*#^j-cae(5#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 CSRF_TRUSTED_ORIGINS = ["https://soccer247gv.com"]
 ALLOWED_HOSTS = ["soccer247gv.com"]
